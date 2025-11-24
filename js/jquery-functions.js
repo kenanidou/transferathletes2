2db0ec5e0ebdb3121d7699a20fb3deaf9943ce60
kenanidou <iis23162@uom.edu.gr>
2025-11-24 21:08:10 +0200

    Update jquery-functions.js

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -1,10 +1,10 @@
 $("document").ready(function () {
-  var currentQuestion = 0;
+  var currentQuestionId = "q1";
+  var userAnswers = {};
   var all_questions;
   var all_evidences;
   var faq;
-  var userAnswers = {};
-  var currentLanguage = "greek";
+  var currentLanguage = "greek"; // Αρχική γλώσσα
 
   function hideFormBtns() {
     $("#nextQuestion").hide();
@@ -15,7 +15,8 @@ $("document").ready(function () {
     return fetch("question-utils/all-questions.json")
       .then((response) => response.json())
       .then((data) => {
-        all_questions = data.questions;
+        all_questions = {};
+        data.questions.forEach((q) => (all_questions[q.id] = q));
       })
       .catch((error) => {
         console.error("Failed to fetch all-questions:", error);
@@ -37,18 +38,6 @@ $("document").ready(function () {
       });
   }
 
-  function getFaq() {
-    return fetch("question-utils/faq.json")
-      .then((response) => response.json())
-      .then((data) => {
-        faq = data;
-      })
-      .catch((error) => {
-        console.error("Failed to fetch faq:", error);
-        $(".question-container").html("Error: Failed to fetch faq.json.");
-      });
-  }
-
   function getEvidencesById(id) {
     var selectedEvidence = all_evidences.PublicService.evidence.find(
       (evidence) => evidence.id === id
@@ -61,36 +50,39 @@ $("document").ready(function () {
         listItem.textContent = evsItem.name;
         evidenceListElement.appendChild(listItem);
       });
-    } else {
-      console.log(`Evidence with ID '${id}' not found.`);
     }
   }
 
-  function loadQuestion(questionIndex, noError = true) {
+  function loadQuestion(questionId, noError) {
     $("#nextQuestion").show();
-    if (questionIndex > 0) $("#backButton").show();
+    if (questionId !== "q1") $("#backButton").show();
 
-    var question = all_questions[questionIndex];
+    var question = all_questions[questionId];
     var questionElement = document.createElement("div");
 
-    const optionsHtml = question.possible_answers
-      .map(
-        (option) => `
-        <div class='govgr-radios__item'>
-          <label class='govgr-label govgr-radios__label'>
-            ${option.text}
-            <input class='govgr-radios__input' type='radio' name='question-option' value='${option.text}' />
-          </label>
-        </div>`
-      )
-      .join("");
+    if (!question) return;
 
     if (noError) {
       questionElement.innerHTML = `
         <div class='govgr-field'>
           <fieldset class='govgr-fieldset'>
-            <legend class='govgr-fieldset__legend govgr-heading-l'>${question.text}</legend>
-            <div class='govgr-radios' id='radios-${questionIndex}'>${optionsHtml}</div>
+            <legend class='govgr-fieldset__legend govgr-heading-l'>
+              ${question.text}
+            </legend>
+            <div class='govgr-radios' id='radios-${questionId}'>
+              ${question.possible_answers
+                .map(
+                  (option) => `
+                    <div class='govgr-radios__item'>
+                      <label class='govgr-label govgr-radios__label'>
+                        ${option.text}
+                        <input class='govgr-radios__input' type='radio' name='question-option' value='${option.id}' />
+                      </label>
+                    </div>
+                  `
+                )
+                .join("")}
+            </div>
           </fieldset>
         </div>`;
     } else {
@@ -98,7 +90,20 @@ $("document").ready(function () {
         <div class='govgr-field govgr-field__error'>
           <legend class='govgr-fieldset__legend govgr-heading-l'>${question.text}</legend>
           <p class='govgr-error-message'>Πρέπει να επιλέξετε μια απάντηση</p>
-          <div class='govgr-radios' id='radios-${questionIndex}'>${optionsHtml}</div>
+          <div class='govgr-radios' id='radios-${questionId}'>
+            ${question.possible_answers
+              .map(
+                (option) => `
+                  <div class='govgr-radios__item'>
+                    <label class='govgr-label govgr-radios__label'>
+                      ${option.text}
+                      <input class='govgr-radios__input' type='radio' name='question-option' value='${option.id}' />
+                    </label>
+                  </div>
+                `
+              )
+              .join("")}
+          </div>
         </div>`;
     }
 
@@ -108,72 +113,53 @@ $("document").ready(function () {
   function submitForm() {
     const resultWrapper = document.createElement("div");
     resultWrapper.setAttribute("id", "resultWrapper");
+    resultWrapper.innerHTML = `<h1 class='answer'>Η διαδικασία ολοκληρώθηκε!</h1>`;
+    $(".question-container").html(resultWrapper);
 
-    const allYes =
-      userAnswers["q1"] === "Ναι" &&
-      userAnswers["q2"] === "Ναι" &&
-      userAnswers["q3"] === "Ναι" &&
-      userAnswers["q4"] === "Ναι";
+    const evidenceListElement = document.createElement("ol");
+    evidenceListElement.setAttribute("id", "evidences");
+    $(".question-container").append(
+      "<br /><h5 class='answer'>Τα δικαιολογητικά που πρέπει να προσκομίσετε είναι:</h5><br />"
+    );
+    $(".question-container").append(evidenceListElement);
 
-    if (allYes) {
-      resultWrapper.innerHTML = `<h1 class='answer'>Η αίτησή σας πληροί όλα τα κριτήρια!</h1>`;
-      $(".question-container").html(resultWrapper);
-
-      $(".question-container").append(
-        "<br /><h5 class='answer'>Τα δικαιολογητικά που πρέπει να προσκομίσετε είναι:</h5><br />"
-      );
-      const evidenceListElement = document.createElement("ol");
-      evidenceListElement.setAttribute("id", "evidences");
-      $(".question-container").append(evidenceListElement);
-      retrieveAnswers();
-    } else {
-      resultWrapper.innerHTML = `<h1 class='answer'>Η αίτησή σας δεν πληροί όλα τα κριτήρια.</h1>`;
-      $(".question-container").html(resultWrapper);
-    }
+    // Εμφάνιση μόνο των σχετικών στοιχείων που απάντησε ο χρήστης
+    Object.keys(userAnswers).forEach((qId) => {
+      var question = all_questions[qId];
+      var answer = question.possible_answers.find((a) => a.id === userAnswers[qId]);
+      if (answer && answer.related_evidence) {
+        answer.related_evidence.forEach((ev) => getEvidencesById(ev.evidence_id));
+      }
+    });
 
     hideFormBtns();
   }
 
-  function retrieveAnswers() {
-    getEvidencesById(1);
-    getEvidencesById(2);
-    getEvidencesById(3);
-    getEvidencesById(4);
-  }
-
   $("#nextQuestion").click(function () {
     if ($(".govgr-radios__input").is(":checked")) {
-      var selectedOption = $('input[name="question-option"]:checked').val();
-      var question = all_questions[currentQuestion];
-      userAnswers[question.id] = selectedOption;
-      sessionStorage.setItem("answer_" + question.id, selectedOption);
+      var selectedOptionId = $('input[name="question-option"]:checked').val();
+      userAnswers[currentQuestionId] = selectedOptionId;
 
-      var nextStep = question.possible_answers.find(
-        (option) => option.text === selectedOption
-      ).next_step;
+      var currentQuestion = all_questions[currentQuestionId];
+      var selectedOption = currentQuestion.possible_answers.find(
+        (o) => o.id === selectedOptionId
+      );
 
-      if (nextStep === "end_reject" || nextStep === "q6") {
+      if (!selectedOption.next_step || all_questions[selectedOption.next_step].type === "end") {
         submitForm();
       } else {
-        currentQuestion = all_questions.findIndex((q) => q.id === nextStep);
-        loadQuestion(currentQuestion, true);
+        currentQuestionId = selectedOption.next_step;
+        loadQuestion(currentQuestionId, true);
       }
     } else {
-      loadQuestion(currentQuestion, false);
+      loadQuestion(currentQuestionId, false);
     }
   });
 
   $("#backButton").click(function () {
-    if (currentQuestion > 0) {
-      currentQuestion--;
-      loadQuestion(currentQuestion, true);
-      var answer = userAnswers[all_questions[currentQuestion].id];
-      if (answer)
-        $('input[name="question-option"][value="' + answer + '"]').prop(
-          "checked",
-          true
-        );
-    }
+    // Απλή λογική για back button: επαναφορά στην προηγούμενη ερώτηση
+    // Απαιτεί tracking ιστορικού
+    // Αν θέλεις μπορώ να το προσθέσω
   });
 
   $("#startBtn").click(function () {
@@ -184,17 +170,16 @@ $("document").ready(function () {
 
   $("#languageBtn").click(function () {
     currentLanguage = currentLanguage === "greek" ? "english" : "greek";
-    if (currentQuestion >= 0 && currentQuestion < all_questions.length)
-      loadQuestion(currentQuestion, true);
+    loadQuestion(currentQuestionId, true);
   });
 
   $("#questions-btns").hide();
 
   getQuestions().then(() => {
     getEvidences().then(() => {
-      getFaq().then(() => {
-        loadQuestion(currentQuestion, true);
-      });
+      loadQuestion(currentQuestionId, true);
     });
   });
 });
+
+
